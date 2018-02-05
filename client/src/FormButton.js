7a0aee36c75ad4ca8713d3f30b3f41c466446db7
noavish <noavish1@gmail.com>
2018-02-05 16:19:17 +0200

    'form'

--- client/src/FormButton.js
+++ client/src/FormButton.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class FormButton extends React.Component{
+    constructor(props){
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(){
+        console.log(this.props.city);
+        this.state.searchCity(this.state);
+    }
+
     render(){
         return(
             <div class="input-group-btn submit-btn-div">
-                <button type="button" class="btn btn-primary btn-default get-temp">Get Temp</button>
+                <button type="button" className="btn btn-default get-temp">Get Temp</button>
             </div>
         );
     }
--- client/src/SearchForm.js
+++ client/src/SearchForm.js
@@ -4,19 +4,36 @@ import FormButton from './FormButton.js';
 class SearchForm extends React.Component{
     constructor(props){
         super(props);
-        this.state = {searchTerm: this.state.city};
+        this.onChange = this.onChange.bind(this);
+        this.state = {city: []};
     }
 
     onChange(e){
-        this.setState({searchTerm: e.target.searchTerm});
+        this.setState({city: e.target.value});
+        console.log(this.state);
+    }
+
+    searchCity(){
+
     }
 
     render(){
         return(
-            <div class="input-group">
-                <input type="text" class="form-control" id="city" placeholder="Enter City" content={this.state.city}/>
-                <FormButton onClick={this.onClick}/>
-            </div>
+            <form action="#" id="getWeatherForm">
+                <div className="input-group">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="city"
+                        placeholder="Enter city"
+                        required
+                        value={this.state.city}
+                        onChange={this.onChange}/>
+                    <span className="input-group-btn">
+                        <FormButton onClick={this.onClick}/>
+                    </span>
+                </div>
+            </form>
         );
     }
 }
